ac04379e218fee353e16f0063146317d974449f4
Elouan Grimm <elouan.grimm@gmail.com>
2025-04-13 14:06:43 +0200

    testing fix?

--- scripts/engine/Editor.js
+++ scripts/engine/Editor.js
@@ -432,7 +432,7 @@
             "/save/localStorage/success",
             Editor.populateSavedSimsDropdown
         );
-    } // END OF Editor.create
+    }; // END OF Editor.create
 
     // Helper to create title headers
     Editor.createTitle = function (html) {
